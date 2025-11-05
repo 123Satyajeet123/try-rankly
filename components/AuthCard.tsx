f084e2b0dcf4bf1fba870bb76592c288c393f8de
Satyajeet Das <satyajeetdas225@gmail.com>
2025-11-05 23:34:10 +0530

    fixed theme

--- components/AuthCard.tsx
+++ components/AuthCard.tsx
@@ -9,6 +9,7 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import Link from 'next/link'
 import { BackgroundBeams } from '@/components/ui/background-beams'
+import { ThemeToggleButton } from '@/components/ThemeToggleButton'
 import { useAuth } from '@/contexts/AuthContext'
 import apiService from '@/services/api'
 
